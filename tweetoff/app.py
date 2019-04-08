8779d8e3d37fe37f327de67cffcbc9d14088286f
trista-paul <trista-paul-linux@LAPTOP-MEEM5KMV.localdomain>
2019-04-08 11:12:21 -0700

    lecture Mon edit of app.py

--- tweetoff/app.py
+++ tweetoff/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from .models import DB
 
 def create_app():
     """create and configure an instance of Flask"""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():
