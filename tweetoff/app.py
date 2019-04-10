2b2133a2faecf6f39d7e4b8b1bcc96660b8db46a
trista-paul <trista-paul-linux@LAPTOP-MEEM5KMV.localdomain>
2019-04-10 09:29:27 -0700

    I forgot the template last upload

--- tweetoff/app.py
+++ tweetoff/app.py
@@ -1,14 +1,25 @@
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template
+from .models import DB, User
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_app():
     """create and configure an instance of Flask"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = getenv('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "WELCOME TO THE TWEETOFF"
+        return render_template('base.html', title='Home', users=User.query.all())
 
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='reset database')
     return app
